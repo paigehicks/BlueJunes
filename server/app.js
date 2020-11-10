1e7be497a18c1c6dea4473fb01a8c160aac622df
Paige Hicks <pnhicks1@gmail.com>
2020-11-09 21:54:38 -0500

    comment out db

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
-  // db
+  // // db
 
-  const express = require('express');
-  const bodyParser = require("body-parser");
-  const app = express();
-  app.use(bodyParser.json());
-  const path = require('path');
+  // const express = require('express');
+  // const bodyParser = require("body-parser");
+  // const app = express();
+  // app.use(bodyParser.json());
+  // const path = require('path');
 
-  const db = require("./db");
-  const collection = "todo";
+  // const db = require("./db");
+  // const collection = "todo";
 
-  db.connect((err)=>{
-    if(err){
-      console.log('unable to connect to database');
-      process.exit(1);
-    }
-    else{
-      app.listen(3000,()=>{
-        console.log('connected to database, app listening on port 3000');
-      });
-    }
-  });
+  // db.connect((err)=>{
+  //   if(err){
+  //     console.log('unable to connect to database');
+  //     process.exit(1);
+  //   }
+  //   else{
+  //     app.listen(3000,()=>{
+  //       console.log('connected to database, app listening on port 3000');
+  //     });
+  //   }
+  // });
